264269b70bc04ed7165bbcac8c7c8ffffeaab158
David Lee <Dai180293@gmail.com>
2025-08-25 09:52:03 +0100

    added additional link to root

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,19 +2,16 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="flex min-h-10 justify-between items-center border-b border-cs-black/25">
-            <p className="hidden min-[455px]:block text-xs text-cs-black/50 pl-2">
-                Turning vision into value.
-            </p>
-            <ul className="flex flex-1 min-[455px]:max-w-80">
-                <li className="flex-1 min-w-40 flex justify-start items-center border-l border-cs-black/25 hover:bg-cs-black hover:text-cs-white">
-                    <Link
-                        href="/information"
-                        className="group h-full flex flex-col gap-1 p-2 w-full"
-                    >
-                        <span className="text-xs 2xl:text-sm text-cs-black/50 group-hover:text-cs-white">More</span>
-                        <div className="text-sm 2xl:text-base space-x-10 flex justify-between">
-                            <p>About me</p>
+        <header className="flex min-h-10 justify-between items-center sm:border-b border-cs-black/10">
+            <p className="text-xs text-cs-black/50 pl-2">Turning vision into value.</p>
+            <ul className="hidden sm:flex">
+                <li className="flex-1 min-w-36 max-w-36 flex justify-start items-center border-l border-cs-black/10 hover:bg-cs-black hover:text-cs-white">
+                    <Link href="/" className="group h-full flex flex-col gap-1 p-2 w-full">
+                        <span className="text-[10px] text-cs-black/50 group-hover:text-cs-white">
+                            Check out
+                        </span>
+                        <div className="text-[13px] font-medium space-x-10 flex justify-between">
+                            <p>My work</p>
                             <div className="relative inline-flex items-center justify-center overflow-hidden">
                                 <div className="translate-x-0 transition group-hover:translate-x-[300%] duration-200">
                                     <svg
@@ -23,7 +20,7 @@ export default function Header() {
                                         viewBox="0 0 15 15"
                                         fill="none"
                                         xmlns="http://www.w3.org/2000/svg"
-                                        className="h-5 w-5"
+                                        className="h-3 w-3"
                                     >
                                         <path
                                             d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
@@ -40,7 +37,7 @@ export default function Header() {
                                         viewBox="0 0 15 15"
                                         fill="none"
                                         xmlns="http://www.w3.org/2000/svg"
-                                        className="h-5 w-5 text-cs-white"
+                                        className="h-3.5 w-3.5 text-cs-white"
                                     >
                                         <path
                                             d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
@@ -54,20 +51,22 @@ export default function Header() {
                         </div>
                     </Link>
                 </li>
-                <li className="flex-1 min-w-40 flex justify-start items-center border-l border-cs-black/25">
-                    <Link href="/contact" className="group h-full flex flex-col gap-1 p-2 w-full">
-                        <span className="text-xs 2xl:text-sm text-cs-black/50">Start a</span>
-                        <div className="text-sm 2xl:text-base space-x-10 flex justify-between">
-                            <p>Conversation</p>
+                <li className="flex-1 min-w-36 max-w-36 flex justify-start items-center border-l border-cs-black/10 hover:bg-cs-black hover:text-cs-white">
+                    <Link href="/information" className="group h-full flex flex-col gap-1 p-2 w-full">
+                        <span className="text-[10px] text-cs-black/50 group-hover:text-cs-white">
+                            More
+                        </span>
+                        <div className="text-[13px] font-medium space-x-10 flex justify-between">
+                            <p>About me</p>
                             <div className="relative inline-flex items-center justify-center overflow-hidden">
-                                <div className="translate-x-0 transition group-hover:translate-x-[300%] duration-500">
+                                <div className="translate-x-0 transition group-hover:translate-x-[300%] duration-200">
                                     <svg
                                         width="15"
                                         height="15"
                                         viewBox="0 0 15 15"
                                         fill="none"
                                         xmlns="http://www.w3.org/2000/svg"
-                                        className="h-5 w-5"
+                                        className="h-3 w-3"
                                     >
                                         <path
                                             d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
@@ -77,14 +76,60 @@ export default function Header() {
                                         ></path>
                                     </svg>
                                 </div>
-                                <div className="absolute -translate-x-[300%] transition group-hover:translate-x-0 duration-500">
+                                <div className="absolute -translate-x-[300%] transition group-hover:translate-x-0 duration-200">
                                     <svg
                                         width="15"
                                         height="15"
                                         viewBox="0 0 15 15"
                                         fill="none"
                                         xmlns="http://www.w3.org/2000/svg"
-                                        className="h-5 w-5"
+                                        className="h-3.5 w-3.5 text-cs-white"
+                                    >
+                                        <path
+                                            d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+                                            fill="currentColor"
+                                            fillRule="evenodd"
+                                            clipRule="evenodd"
+                                        ></path>
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                </li>
+                <li className="flex-1 min-w-36 max-w-36 flex justify-start items-center border-l border-cs-black/10 hover:bg-cs-black hover:text-cs-white">
+                    <Link href="/" className="group h-full flex flex-col gap-1 p-2 w-full">
+                        <span className="text-[10px] text-cs-black/50 group-hover:text-cs-white">
+                            Start a
+                        </span>
+                        <div className="text-[13px] font-medium space-x-10 flex justify-between">
+                            <p>Conversation</p>
+                            <div className="relative inline-flex items-center justify-center overflow-hidden">
+                                <div className="translate-x-0 transition group-hover:translate-x-[300%] duration-200">
+                                    <svg
+                                        width="15"
+                                        height="15"
+                                        viewBox="0 0 15 15"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-3 w-3"
+                                    >
+                                        <path
+                                            d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+                                            fill="currentColor"
+                                            fillRule="evenodd"
+                                            clipRule="evenodd"
+                                        ></path>
+                                    </svg>
+                                </div>
+                                <div className="absolute -translate-x-[300%] transition group-hover:translate-x-0 duration-200">
+                                    <svg
+                                        width="15"
+                                        height="15"
+                                        viewBox="0 0 15 15"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-3.5 w-3.5 text-cs-white"
                                     >
                                         <path
                                             d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
